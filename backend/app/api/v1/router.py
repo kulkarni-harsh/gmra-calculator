a6afa59033c42e0f78d296e87c8958f9dc4a3ee7
Harsh Kulkarni <harshsk17@gmail.com>
2026-03-16 00:57:36 +0530

    feat: delete v1

--- backend/app/api/v1/router.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.v1.endpoints import slides
-
-router = APIRouter()
-# router.include_router(health.router, tags=["health"])
-router.include_router(slides.router, tags=["slides"], prefix="/slides")
